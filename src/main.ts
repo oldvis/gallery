8725cfa9c0fbe620d626dc961cd67a9b4eda64ee
Yu Zhang <yuzhang94@outlook.com>
2025-11-06 16:05:28 +0100

    feat: add ga

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import routes from 'virtual:generated-pages'
 import { createApp } from 'vue'
+import { configure } from 'vue-gtag'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
@@ -9,6 +10,10 @@ import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import './styles/scrollbar.css'
 import 'uno.css'
+
+configure({
+  tagId: import.meta.env.VITE_GA_ID,
+})
 
 const app = createApp(App)
 const router = createRouter({
